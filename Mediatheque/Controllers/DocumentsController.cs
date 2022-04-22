b36a2b3d44d429760b26c6c7ee515e96f977fb06
Oueslati Ahmed Khalil <45701691+kamatchokhalil@users.noreply.github.com>
2022-04-22 23:52:57 +0100

    Add project files.

---
+++ Mediatheque/Controllers/DocumentsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Domain.Entities;
+using Service;
+
+namespace Mediatheque.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        private readonly Context _context;
+        private readonly IDocumentService _service;
+
+        public DocumentsController(Context context,IDocumentService service)
+        {
+            _context = context;
+            _service = service;
+        }
+
+        // GET: Documents
+        public IActionResult Index()
+        {
+            return View(_service.GetMany());
+        }
+
+        // GET: Documents/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var document = await _context.Documents
+                .FirstOrDefaultAsync(m => m.Key == id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            return View(document);
+        }
+
+        // GET: Documents/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Documents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Key,Titre,Auteur,Annee")] Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(document);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(document);
+        }
+
+        // GET: Documents/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var document = await _context.Documents.FindAsync(id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+            return View(document);
+        }
+
+        // POST: Documents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Key,Titre,Auteur,Annee")] Document document)
+        {
+            if (id != document.Key)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(document);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DocumentExists(document.Key))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(document);
+        }
+
+        // GET: Documents/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var document = await _context.Documents
+                .FirstOrDefaultAsync(m => m.Key == id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            return View(document);
+        }
+
+        // POST: Documents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var document = await _context.Documents.FindAsync(id);
+            _context.Documents.Remove(document);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DocumentExists(int id)
+        {
+            return _context.Documents.Any(e => e.Key == id);
+        }
+    }
+}
---
+++ Service/EmpruntService.cs
@@ -0,0 +1,101 @@
+﻿using Domain.Entities;
+using PS.Data.Infrastructure;
+using ServicePattern;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class EmpruntService : Service<Emprunt>, IEmpruntService
+    {
+        List<Document> documents;
+        public IUnitOfWork uiprop { get; set; }
+
+        public EmpruntService(IUnitOfWork ui) : base(ui)
+        {
+            uiprop = ui;
+        }
+
+        public int NbEmpruntsEffectuees(Client client)
+        {
+            List<Emprunt> empList = (List<Emprunt>)this.GetMany(emp => emp.ClientFk == client.ClientId);
+            return empList.Count;
+        }
+
+        public int NbEmpruntsEnCours(Client client)
+        {
+
+            // check Date retour
+            List<Emprunt> empList = (List<Emprunt>)this.GetMany(emp => emp.ClientFk == client.ClientId && emp.DateLimite < DateTime.Now);
+            return empList.Count;
+        }
+
+        public int NbEmpruntsDeposes(Client client)
+        {
+            List<Emprunt> empList = (List<Emprunt>)this.GetMany(emp => emp.ClientFk == client.ClientId);
+            return empList.Count;
+        }
+
+        public int NbEmprunts(Document doc)
+        {
+            List<Emprunt> empList = (List<Emprunt>)this.GetMany(emp => emp.DocumentFk == doc.Key);
+            return empList.Count;
+        }
+
+        public void Emprunter(Document doc, Client client)
+        {
+            List<Document> docEmpruntable = new List<Document>();
+
+            foreach (var item in documents)
+            {
+                if (item.Empruntable && doc.Equals(item))
+                {
+                    int nbr = item.NbEmprunts;
+                    Emprunt emp = new Emprunt();
+                    emp.DateEmprunt = DateTime.Now;
+                    emp.DateLimite = DateTime.Now.AddDays(8);
+                    emp.DateRappel = DateTime.Now.AddDays(5);
+                    emp.DateRetour = DateTime.Now.AddDays(7);
+                    emp.Tarif = 3d;
+
+                    emp.ClientFk = client.ClientId;
+                    emp.DocumentFk = item.Key;
+
+                    this.Add(emp);
+
+                    item.NbEmprunts = nbr + 1;
+                    item.Empruntable = false;
+                    item.Emprunte = true;
+                }
+
+            }
+
+        }
+
+        public void Rendre(Document doc)
+        {
+            DocumentService documentService = new DocumentService(uiprop);
+        
+            if (doc != null)
+            {
+
+                //get and remove from table Emprunt
+                Emprunt emp = this.Get(emp => emp.DocumentFk == doc.Key);
+                this.Delete(emp);
+
+                // update attributs of document
+                // not mapped 
+                // ToDo
+                doc.Empruntable = true;
+                doc.Emprunte = false;
+                documentService.Update(doc);
+
+            }
+            else
+            {
+                throw new Exception("Document not found");
+            }
+        }
+    }
+}
